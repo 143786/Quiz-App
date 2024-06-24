b282715ed5fe1e98b56d02a3e64f6134c0a572f7
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-06-24 20:42:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+  {
+    questions: "What is a group of unicorns known as?",
+    answers: [{ text: "a herd ", correct: false }],
+  },
+];
+
+// A. A herd
+// B. A shimmer✅
+// C. A crowd
+// D. A rainbow
