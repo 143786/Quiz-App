19c5142cdbecc66dc7f878a9a72ee22c9c23bf1d
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-06-25 02:17:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const questions = [
   {
-    questions: "What is a group of unicorns known as?",
+    question: "What is a group of unicorns known as?",
     answers: [
       { text: "A herd ", correct: false },
       { text: "A crowd ", correct: false },
@@ -9,7 +9,7 @@ const questions = [
     ],
   },
   {
-    questions: "What is the fear of fun called?",
+    question: "What is the fear of fun called?",
     answers: [
       { text: "Phobophobia", correct: false },
       { text: "Cherophobia ", correct: true },
@@ -19,7 +19,7 @@ const questions = [
   },
 
   {
-    questions: "Which country invented ice cream?",
+    question: "Which country invented ice cream?",
     answers: [
       { text: "USA", correct: false },
       { text: "Italy ", correct: false },
@@ -29,7 +29,7 @@ const questions = [
   },
 
   {
-    questions: "Which animal sleeps the most?",
+    question: "Which animal sleeps the most?",
     answers: [
       { text: "Sloth", correct: false },
       { text: "Cat ", correct: false },
@@ -39,7 +39,7 @@ const questions = [
   },
 
   {
-    questions: "Which ancient people invented the toothbrush??",
+    question: "Which ancient people invented the toothbrush??",
     answers: [
       { text: "Romans", correct: false },
       { text: "Egyptians", correct: true },
@@ -50,7 +50,7 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -59,19 +59,76 @@ let score = 0;
 function startQuiz() {
   currentQuestionIndex = 0;
   score = 0;
-  nextButton.innerHTML = " Next";
-  showQuestions();
+  nextButton.innerHTML = "Next";
+  showQuestion();
 }
 
-function showQuestions() {
+function showQuestion() {
+  resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
-  questionElement.innerHTML = questionNo + ". " + currentQuestion.questions;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
   currentQuestion.answers.forEach((answer) => {
-    const button = document.correctElement("button");
+    const button = document.createElement("button");
     button.innerHTML = answer.text;
     button.classList.add("btn");
-    answerButton.appendChild(button);
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
   });
 }
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored $ {score} out of ${questions.length}!`;
+
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();
